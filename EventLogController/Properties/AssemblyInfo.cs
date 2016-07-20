0056aadb8e94c6ddab317ee586ea8b3816063129
amenayach <amenprog@gmail.com>
2016-07-20 13:01:47 +0300

    Update assembly info

--- EventLogController/Properties/AssemblyInfo.cs
+++ EventLogController/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EventLogController")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EventLogController by amen ayach")]
+[assembly: AssemblyDescription("EventLogController by amen ayach")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EventLogController")]
+[assembly: AssemblyCompany("amen ayach")]
+[assembly: AssemblyProduct("EventLogController by amen ayach")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("amen ayach")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
